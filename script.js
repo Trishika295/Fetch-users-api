d171d261d1984dbf42e97dab0d54287a9d77fd7a
Trishika295 <trishikas295@gmail.com>
2026-02-26 20:11:06 +0530

    fetch
    Refactor fetchUsers function to improve readability and structure.

--- script.js
+++ script.js
@@ -3,22 +3,16 @@ const reloadBtn = document.getElementById("reloadBtn");
 
 async function fetchUsers() {
     container.innerHTML = "Loading...";
-
     try {
         const response = await fetch("https://jsonplaceholder.typicode.com/users");
-
         if (!response.ok) {
             throw new Error("Network response was not ok");
         }
-
         const users = await response.json();
-
         container.innerHTML = "";
-
         users.forEach(user => {
             const card = document.createElement("div");
             card.className = "user-card";
-
             card.innerHTML = `
                 <h3>${user.name}</h3>
                 <p><strong>Email:</strong> ${user.email}</p>
@@ -26,17 +20,11 @@ async function fetchUsers() {
                 ${user.address.street}, 
                 ${user.address.city}</p>
             `;
-
             container.appendChild(card);
         });
-
     } catch (error) {
         container.innerHTML = `<p style="color:red;">Error: ${error.message}</p>`;
     }
 }
-
-// Load data initially
 fetchUsers();
-
-// Reload button
 reloadBtn.addEventListener("click", fetchUsers);
